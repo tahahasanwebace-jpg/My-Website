5c599df08dcc99cc5829e1742f19bf55c6b8cde5
tahahasanwebace-jpg <tahahasanwebace@gmail.com>
2025-09-08 11:23:25 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function moverandomE1(elm) {
+    elm.style.position = "absolute";
+    elm.style.top = Math.floor(Math.random() * 90 + 5) + "%";
+    elm.style.left = Math.floor(Math.random() * 90 + 5) + "%";
+
+}
+
+
+const moverandom = document.querySelector("#move-random");
+
+moverandom.addEventListener("mouseenter", function(e) {
+    moverandomE1(e.target);
+});
